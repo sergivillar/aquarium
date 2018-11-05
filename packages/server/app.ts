1c56f32d0bfb16433a8f5db9114c7a87c63c155e
Sergio Fernández <sergio.fernandezj@gmail.com>
2018-11-05 21:45:21 +0100

    moving routes

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -5,7 +5,6 @@ import {ApolloServer} from 'apollo-server-express';
 import schema from './src/graphql';
 import routes from './src/routes';
 import {errorMiddleware} from './src/utils/middleware';
-import APIError from './src/errors';
 
 if (process.env.NODE_ENV !== 'production') {
     dotenv.config();
@@ -19,26 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(passport.initialize());
-
 app.use('/', routes);
-
-app.post('/graphql', (req, res, next) => {
-    passport.authenticate('jwt', {session: false}, (err, user, info) => {
-        if (err || info) {
-            return next(new APIError(err || info.message || info, 401));
-        }
-
-        if (!user) {
-            return next(new APIError('JWT not related to any user', 401));
-        }
-
-        if (user) {
-            req.user = user;
-        }
-
-        next();
-    })(req, res, next);
-});
 
 const apolloServer = new ApolloServer({
     schema,
@@ -46,8 +26,8 @@ const apolloServer = new ApolloServer({
         user: req.user,
     }),
 });
-apolloServer.applyMiddleware({app});
 
+apolloServer.applyMiddleware({app});
 app.use(errorMiddleware);
 
 export default app;
--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -27,4 +27,22 @@ router.post('/login', (req: Request, res: Response, next) => {
     })(req, res);
 });
 
+router.post('/graphql', (req, res, next) => {
+    passport.authenticate('jwt', {session: false}, (err, user, info) => {
+        if (err || info) {
+            return next(new APIError(err || info.message || info, 401));
+        }
+
+        if (!user) {
+            return next(new APIError('JWT not related to any user', 401));
+        }
+
+        if (user) {
+            req.user = user;
+        }
+
+        next();
+    })(req, res, next);
+});
+
 export default router;
