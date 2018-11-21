4053a75ae427f09f843fd2f7b58955307568159d
sfernandez <sfernandez@tuenti.com>
2018-11-21 11:17:33 +0100

    fixed types

--- packages/server/src/graphql/resolvers/measure.ts
+++ packages/server/src/graphql/resolvers/measure.ts
@@ -1,9 +1,7 @@
 import Sequelize from 'sequelize';
 import models from '../../models';
 import {MeasureInstance} from '../../models/measure';
-import {CreateMeasureMutationArgs, GetMeasuresQueryArgs} from '../../typings/generated';
-
-import {MeasurePaginated, Measure} from '../../typings/generated';
+import {CreateMeasureMutationArgs, GetMeasuresQueryArgs,PageInfo} from '../../typings/generated';
 
 const toCursorHash = (value: string) => Buffer.from(value).toString('base64');
 const fromCursorHash = (value: string) => Buffer.from(value, 'base64').toString('ascii');
@@ -13,15 +11,15 @@ export default {
         getMeasures: async (
             _: any,
             {aquariumId, cursor, limit = 5}: GetMeasuresQueryArgs | any
-        ): Promise<MeasurePaginated> => {
-            const measures = ((await models.Measure.findAll({
+        ): Promise<{measures: MeasureInstance[], pageInfo: PageInfo}> => {
+            const measures = await models.Measure.findAll({
                 order: [['createdAt', 'DESC']],
                 limit: limit + 1,
                 where: {
                     aquariumId,
                     ...(cursor && {createdAt: {[Sequelize.Op.lt]: fromCursorHash(cursor)}}),
                 },
-            })) as any) as Measure[];
+            });
 
             const hasNextPage = measures.length > limit;
             const result = hasNextPage ? measures.slice(0, -1) : measures;
