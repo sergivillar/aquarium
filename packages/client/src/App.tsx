0f1f4c28940d1186509d5cdc499ca94e569fe662
Sergio Fernández <sergio.fernandezj@gmail.com>
2018-12-02 13:15:02 +0100

    create client

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`;
+
+const App = () => <Title>Hello World!</Title>;
+
+export default App;
